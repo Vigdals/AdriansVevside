ed969562dbc6c38872d52143aba2f5eb49211816
Adrian Høyheim Vigdal <adhvi@fylkesmannen.no>
2023-02-16 10:17:26 +0100

    minors

--- Adrians/Program.cs
+++ Adrians/Program.cs
@@ -16,6 +16,8 @@ builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.Requ
 builder.Services.AddControllersWithViews();
 
 
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
