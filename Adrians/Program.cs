d43f9e21b8dcda6288a570882fb58a201c9b74f5
Vigdals <adrvig92@gmail.com>
2023-06-12 18:47:54 +0200

    nothing new actually

--- Adrians/Program.cs
+++ Adrians/Program.cs
@@ -15,9 +15,6 @@ builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.Requ
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
 
-
-
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
