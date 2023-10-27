c3eb42cffdd871785f99477957e13d38ae34814f
FYLKESMANNEN\adhvi <adhvi@statsforvaltaren.no>
2023-10-27 10:40:07 +0200

    added enhetsregister stuff and a blog function?

---
+++ Adrians/Controllers/EnhetsregisteretController.cs
@@ -0,0 +1,34 @@
+﻿using Adrians.Resources;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace Adrians.Controllers
+{
+    public class EnhetsregisteretController : Controller
+    {
+        public IActionResult Index(string orgNummer)
+        {
+            if (string.IsNullOrWhiteSpace(orgNummer))
+            {
+                return View();
+            }
+            else
+            {
+                var Organisasjon = GetApiInfo("https://data.brreg.no/enhetsregisteret/api/enheter/" + orgNummer);
+
+                return View();
+            }
+        }
+
+        public async Task<List<OrgModel>> GetApiInfo(string apiEndpoint)
+        {
+            var OrgModelList = new List<OrgModel>();
+            var jsonResult = await ApiCall.DoApiCallAsync(apiEndpoint);
+
+            JsonElement json = JsonSerializer.Deserialize<JsonElement>(jsonResult);
+
+
+            return OrgModelList;
+        }
+    }
+}
---
+++ Adrians/Models/OrgModel.cs
@@ -0,0 +1,18 @@
+﻿namespace Adrians.Models
+{
+    public class OrgModel
+    {
+        public string? organisasjonsnummer { get; set; }
+        public string? navn { get; set; }
+        public string? registreringsdatoEnhetsregisteret { get; set; }
+        public bool? registrertIMvaregisteret { get; set; }
+        public int? antallAnsatte { get; set; }
+        public bool? registrertIForetaksregisteret { get; set; }
+        public bool? registrertIStiftelsesregisteret { get; set; }
+        public bool? registrertIFrivillighetsregisteret { get; set; }
+        public bool? konkurs { get; set; }
+        public bool? underAvvikling { get; set; }
+        public bool? underTvangsavviklingEllerTvangsopplosning { get; set; }
+        public string? maalform { get; set; }
+    }
+}
