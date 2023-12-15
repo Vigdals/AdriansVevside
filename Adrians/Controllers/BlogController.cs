05c3288325a0ed67f7f55ef93d53ddb057644e28
FYLKESMANNEN\adhvi <adhvi@statsforvaltaren.no>
2023-12-15 10:55:02 +0100

    Added new countdown and a new logo

--- Adrians/Controllers/BlogController.cs
+++ Adrians/Controllers/BlogController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using Azure.Storage.Blobs;
 
 namespace Adrians.Controllers
 {
@@ -6,7 +9,32 @@ namespace Adrians.Controllers
     {
         public IActionResult Index()
         {
+            
             return View();
+        }
+
+        private string[] permittedExtensions = { ".txt", ".pdf" };
+        private long maxFileSize = 1024 * 15;
+        private async void LoadFile(InputFileChangeEventArgs e)
+        {
+            if (e.File != null)
+            {
+                if (e.File.Size == 0 || e.File.Size > maxFileSize)
+                {
+                    // log error
+                }
+                else
+                {
+                    var ext = Path.GetExtension(e.File.Name).ToLowerInvariant();
+
+                    if (string.IsNullOrEmpty(ext) || !permittedExtensions.Contains(ext))
+                    {
+                        var trustedFileNameForFileStorage = Path.GetRandomFileName();
+
+                        await new BlobContainerClient("connection", "blob").UploadBlobAsync(trustedFileNameForFileStorage, e.File.OpenReadStream());
+                    }
+                }
+            }
         }
     }
 }
