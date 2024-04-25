346ae7cc44c08c838161d55e918fd30b5ef55bff
Adrian Høyheim Vigdal <adrian.vigdal@statsforvalteren.no>
2024-04-25 21:23:24 +0200

    EM24

---
+++ Adrians/Controllers/EM24Controller.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Adrians.Controllers
+{
+    public class EM24Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
