9f661ab7bcbc9ea8684f2ee4b58e880db57e8411
FYLKESMANNEN\adhvi <adhvi@statsforvaltaren.no>
2024-04-26 00:18:57 +0200

    added EM24 matches and groups

--- Adrians/Controllers/EM24Controller.cs
+++ Adrians/Controllers/EM24Controller.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using System.Text.Json;
+using static Adrians.Models.TournamentModel;
 
 namespace Adrians.Controllers
 {
@@ -14,31 +15,44 @@ namespace Adrians.Controllers
         public IActionResult Index(string tournamentID)
         {
             tournamentID = "59";
-
-            Debug.WriteLine(tournamentID);
-            if (string.IsNullOrWhiteSpace(tournamentID))
+            //Henter ut info om heile turneringa. Kvar gruppe har ein ID som treng eit api kall for å henta alle kampar
+            var TournamentViewModelList = GetTournamentInfo(tournamentID);
+            //Oppretta tom liste av kampar
+            var matchViewModelList = new List<NifsKampViewModel>();
+            //Går igjennom kvar gruppe og hentar ut alle kampar
+            foreach (var tournamentViewModel in TournamentViewModelList)
             {
-                return View();
-            }
-            else
-            {
-                var tournamentModels = GetGruppeInfo("https://api.nifs.no/tournaments/" + tournamentID + "/stages/");
-                //var matchModels = GetKampInfo("https://api.nifs.no/stages/690256/matches/");
-
-
-                var TournamentViewModelList = new List<TournamentViewModel>();
+                var matchModels = GetKampInfo("https://api.nifs.no/stages/" + tournamentViewModel.id + "/matches/");
 
                 //adding info just to display to the view
-                foreach (var gruppe in tournamentModels.Result)
+                foreach (var match in matchModels.Result)
                 {
-                    if (gruppe.yearStart == 2024)
-                    {
-                        TournamentViewModelList.Add(new TournamentViewModel(gruppe));
-                    }
+                    matchViewModelList.Add(new NifsKampViewModel(match));
+                    Debug.WriteLine(match.homeTeam.name + " + " + match.awayTeam.name);
                 }
-                return View(TournamentViewModelList);
+                //return View(matchViewModelList);
+            }
+            return View(matchViewModelList);
+        }
+
+        public List<TournamentViewModel> GetTournamentInfo(string tournamentID)
+        {
+            var tournamentModels = GetGruppeInfo("https://api.nifs.no/tournaments/" + tournamentID + "/stages/");
+
+            var TournamentViewModelList = new List<TournamentViewModel>();
+
+            //adding info just to display to the view
+            foreach (var gruppe in tournamentModels.Result)
+            {
+                if (gruppe.yearStart == 2024)
+                {
+                    TournamentViewModelList.Add(new TournamentViewModel(gruppe));
+                }
             }
 
+
+
+            return TournamentViewModelList;
         }
 
         public async Task<List<TournamentModel.Root>> GetGruppeInfo(string apiEndpoint)
@@ -50,6 +64,18 @@ namespace Adrians.Controllers
             var tournamentModels = JsonSerializer.Deserialize<List<TournamentModel.Root>>(jsonResult);
 
             return tournamentModels;
+        }
+
+        public async Task<List<NifsKampModel>> GetKampInfo(string apiEndpoint)
+        {
+            // string apiUrl = "https://api.nifs.no/stages/690256/matches/";
+            var jsonResult = await ApiCall.DoApiCallAsync(apiEndpoint);
+
+            //The most sexy oneliner in the world!
+            //Takes the jsonResult, deserializes it and adds it to my model. Crazy easy
+            var matchModel = JsonSerializer.Deserialize<List<NifsKampModel>>(jsonResult);
+
+            return matchModel;
         }
     }
 }
