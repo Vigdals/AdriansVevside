5fc4f13d6213ec77b57ccdebbc73973d037f13c3
FYLKESMANNEN\adhvi <adhvi@statsforvaltaren.no>
2024-12-05 14:40:42 +0100

    added Application Insights packages and lines in program.cs

--- Adrians/Program.cs
+++ Adrians/Program.cs
@@ -5,6 +5,8 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddApplicationInsightsTelemetry();
+
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
