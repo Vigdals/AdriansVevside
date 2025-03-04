5c8688ecff89ded103e7f60d4d63027729688edf
sf\adhvi <adhvi@fylkesmannen.no>
2025-03-04 08:30:14 +0100

    Added logos for the view

--- Adrians/Models/BarcelonaModel.cs
+++ Adrians/Models/BarcelonaModel.cs
@@ -1,8 +1,28 @@
-using System;
 using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace Adrians.Models
 {
+    public class BarcelonaModel
+    {
+        public List<MatchDetail> Matches { get; set; }
+    }
+
+    public class MatchDetail
+    {
+        public Team HomeTeam { get; set; }
+        public Team AwayTeam { get; set; }
+        public string utcDate { get; set; }
+        public string Status { get; set; }
+    }
+
+    public class Team
+    {
+        public string Name { get; set; }
+        [JsonProperty("crestUrl")]
+        public string CrestUrl { get; set; } // Property for the team logo URL
+    }
+
     public class Match
     {
         public string HomeTeam { get; set; }
@@ -13,4 +33,10 @@ namespace Adrians.Models
         public string AwayTeamLogo { get; set; }  // Logo URL for the away team
     }
 
+    public class Player
+    {
+        public string Name { get; set; }
+        public string Position { get; set; }
+        public string Team { get; set; }
+    }
 }
--- Adrians/Resources/FotballDataApi.cs
+++ Adrians/Resources/FotballDataApi.cs
@@ -1,13 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Net.Http;
-using System.Threading.Tasks;
 using Newtonsoft.Json;
+using Adrians.Models;
+using System.Diagnostics;
 
 public class FotballDataApi
 {
-    private const string ApiUrl = "https://api.football-data.org/v4/teams/81/matches";  // Correct URL for FC Barcelona
-    private readonly string _apiKey = "4f5a263e32034f429ec2bf5b3cfce2b2";  // Your API key
+    private const string ApiUrl = "https://api.football-data.org/v4/teams/81/matches"; // Correct URL for FC Barcelona
+    private readonly string _apiKey = "4f5a263e32034f429ec2bf5b3cfce2b2"; // Your API key
 
     public async Task<List<Match>> GetUpcomingMatchesAsync()
     {
@@ -18,44 +16,34 @@ public class FotballDataApi
             // Fetch matches for FC Barcelona with the correct API call
             var response = await client.GetStringAsync($"{ApiUrl}?status=SCHEDULED&limit=5");
 
-            // Deserialize the response into the list of Match objects
-            var matchesResponse = JsonConvert.DeserializeObject<ApiMatchesResponse>(response);
+            // Log the response for debugging
+            Debug.WriteLine(response);
+
+            // Deserialize the response into the BarcelonaModel object
+            var matchesResponse = JsonConvert.DeserializeObject<BarcelonaModel>(response);
+
+            // Log the deserialized response for debugging
+            Debug.WriteLine(JsonConvert.SerializeObject(matchesResponse, Formatting.Indented));
 
             var upcomingMatches = new List<Match>();
 
-            foreach (var match in matchesResponse.Matches)
+            if (matchesResponse?.Matches != null)
             {
-                upcomingMatches.Add(new Match
+                foreach (var match in matchesResponse.Matches)
                 {
-                    HomeTeam = match.HomeTeam.Name,
-                    AwayTeam = match.AwayTeam.Name,
-                    Date = match.utcDate,
-                    Status = match.Status,
-                    HomeTeamLogo = match.HomeTeam.CrestUrl,  // Adding logo URL
-                    AwayTeamLogo = match.AwayTeam.CrestUrl   // Adding logo URL
-                });
+                    upcomingMatches.Add(new Match
+                    {
+                        HomeTeam = match.HomeTeam.Name,
+                        AwayTeam = match.AwayTeam.Name,
+                        Date = match.utcDate,
+                        Status = match.Status,
+                        HomeTeamLogo = match.HomeTeam.CrestUrl, // Adding logo URL
+                        AwayTeamLogo = match.AwayTeam.CrestUrl // Adding logo URL
+                    });
+                }
             }
 
             return upcomingMatches;
         }
     }
-}
-
-public class ApiMatchesResponse
-{
-    public List<MatchDetail> Matches { get; set; }
-}
-
-public class MatchDetail
-{
-    public Team HomeTeam { get; set; }
-    public Team AwayTeam { get; set; }
-    public string utcDate { get; set; }
-    public string Status { get; set; }
-}
-
-public class Team
-{
-    public string Name { get; set; }
-    public string CrestUrl { get; set; }  // Property for the team logo URL
 }
